388023e665dbe928c3e40e49fa47c2b4ffcd7f34
Joseph <JSPenland97@gmai.com>
2021-01-09 10:27:57 -0500

    test

--- javascript.js
+++ javascript.js
@@ -1,20 +1,12 @@
-import populationIO from 'api-population-io';
-const populationIO = require('api-population-io');
-
-populationIO.lifeExpectancy.total({
-        sex: 'male',
-        country: 'United Kingdom',
-        dob: '1970-01-01'
-    })
-    .then((data) => {
-        console.log('total_life_expectancy:', data.total_life_expectancy);
-    })
-    .catch((err) => {
-        console.log(err.detail);
-    });
-response = unirest.get("https://daxeel-celebinfo-v1.p.mashape.com/date/28/september",
+response = unirest.get("https://daxeel-celebinfo-v1.p.mashape.com/date/" + day + "/" + month,
     headers = {
         "X-Mashape-Key": "V96M0xptxxxxxx",
         "Accept": "text/plain"
     }
 )
+var results = response.data;
+
+for (var i = 0; i < results.length; i++) {
+
+
+}
